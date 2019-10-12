eed67d267e5984a70dc6d40c2b44be09d5af54ba
txazo <784990655@qq.com>
2019-10-12 18:38:52 +0800

    auto commit

---
+++ user/src/main/java/org/txazo/user/Application.java
@@ -0,0 +1,15 @@
+package org.txazo.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.txazo.im.common.util.Log4jUtil;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        Log4jUtil.initConfig();
+        SpringApplication.run(Application.class, args);
+    }
+
+}
